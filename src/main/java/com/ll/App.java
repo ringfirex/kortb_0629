dbbf9c5e62b32a3f8e1314ef3b7b2bec5ee323f8
ringfirex <ringfirex@gmail.com>
2024-07-01 03:02:35 +0900

    스캐너 도입

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,7 +1,19 @@
 package com.ll;
 
+import java.util.Scanner;
+
 class App {
   void run() {
     System.out.println("프로그램 실행");
+    System.out.println("== 명언 앱 ==");
+
+    System.out.print("명령) ");
+
+    Scanner scanner = new Scanner(System.in);
+    String cmd = scanner.nextLine();
+
+    System.out.printf("입력하신 명령 : %s\n", cmd);
+    //위 라인과 아래 라인과 같은 의미
+    // System.out.print("입력하신 명령 : " + cmd + "\n");
   }
 }
