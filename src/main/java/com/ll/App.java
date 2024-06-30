68571d24a6142afe22c672464a1d04d49f088c9b
ringfirex <ringfirex@gmail.com>
2024-07-01 03:24:15 +0900

    2단계 완료

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -16,9 +16,15 @@ class App {
       if (cmd.equals("종료")) {
         break;
       }
+      else if (cmd.equals("등록")) {
+        System.out.print("명언 : ");
+        String content = scanner.nextLine();
 
+        System.out.print("작가 : ");
+        String authorName = scanner.nextLine();
 
-      System.out.printf("입력하신 명령 : %s\n", cmd);
+        System.out.printf("명언 : %s, 작가 : %s\n", content, authorName);
+      }
     }
   }
 }
